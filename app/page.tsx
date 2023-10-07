ce546be412da01101d7a25543c8bc68b2860dd41
VatsaDev <vatsadev123@gmail.com>
2023-10-07 17:49:54 -0500

    UPD

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Tasks</h1>
-      </div>
+    <main className="min-h-screen">
+      <h1 className='text-8xl'>TaskList</h1>
+      <ul>
+        <li>Prioritizing Tasks</li>
+        <li>Cool UI</li>
+        <li>Noice</li>
+      </ul>
     </main>
   )
 }
